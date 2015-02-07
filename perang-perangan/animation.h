cb2d4f3403febc20e1dfb42a8eec9d7d787ce8eb
Eric <eric2861994@gmail.com>
2015-02-07 13:52:24 +0700

    added abstract class animation

---
+++ perang-perangan/animation.h
@@ -0,0 +1,18 @@
+#include "../core/point.h"
+#include <ctime>
+
+class Animation {
+	public:
+		virtual void draw() = 0;
+		virtual void update(time_t timeElapsed) = 0;
+		void setTopLeftPosition(int x, int y);
+		void setWidth(int width);
+		void setHeight(int height);
+		Point getTopLeftPosition() const;
+		Point getBottomRight() const;
+		bool isCollide(const Animation& other);
+
+	private:
+		int width, height;
+		Point topLeftPosition;
+};
