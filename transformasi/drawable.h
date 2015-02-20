5034282d7cd22ed58dd1b7332a472511104f1661
Winson <winson_w94@hotmail.com>
2015-02-20 21:33:02 +0700

    Refactor Animation to Drawable

---
+++ transformasi/drawable.h
@@ -0,0 +1,20 @@
+#ifndef DRAWABLE_H
+#define DRAWABLE_H
+
+#include "../core/point.h"
+
+class Drawable {
+	public:
+		virtual void draw() = 0;
+		
+		virtual Point getPosition() const = 0;
+		virtual void setPosition() = 0;
+		virtual void move(int dx, int dy) = 0;
+		
+		virtual int getWidth() const = 0;
+		virtual int getHeight() const = 0;
+				
+		friend bool isCollide(const Drawable& a, const Drawable& b);
+};
+
+#endif
