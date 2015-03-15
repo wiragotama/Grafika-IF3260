730f5615f6eae9962c25943f0895faab35c66c1a
willy2706 <willylie2706@gmail.com>
2015-03-15 16:48:56 +0700

    bisa clipping di 3d

--- peta3d/peta3d.h
+++ peta3d/peta3d.h
@@ -10,21 +10,34 @@
 #include <vector>
 #include <algorithm>
 
+typedef int OutCode;
+
 class Peta3D {
 public:
 	Peta3D();
 	void drawPeta(Canvas*);
 	void drawPeta3d(Canvas* canvas);
-	
+	void drawPetaClipping(Canvas* canvas);
+
 private:
 	void loadPeta2d(const char*);
 	vector<Polygon> polygonTo3D(Polygon* polygon, int dy);
 	void generetePeta3dSurfaces();
 	void generetePeta3dFromSurface();
-	
+	void CohenSutherlandLineClipAndDraw(Point p0, Point p1, Canvas* canvas);
+	OutCode ComputeOutCode(int x, int y, int xmin, int ymin, int xmax, int ymax);
+	void scaleAndDraw(Canvas* canvas, Point p0, Point p1);
+
 private:
 	vector<Polygon> peta2d;
 	vector<Polygon> petaSurface;
 	vector<Line> peta3d;
+	Polygon viewFrame;
+	Polygon highlightedArea;
+	const int INSIDE; // 0000
+	const int LEFT;   // 0001
+	const int RIGHT;  // 0010
+	const int BOTTOM; // 0100
+	const int TOP;    // 1000
 };
 #endif
